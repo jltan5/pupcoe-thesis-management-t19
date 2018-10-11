994c905445d89ac23073d052966814390a2c336d
Team 19 <tanjohnloyd3@gmail.com>
2018-10-11 19:38:26 +0800

    module 8

--- app.js
+++ app.js
@@ -32,15 +32,155 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 app.get('/', function (req, res) {
   res.render('login');
 });
 
+app.get('/Faculty/Admin/Home', function (req, res) {
+  res.render('adminhome');
+});
 
+app.get('/Faculty/Regular/Home', function (req, res) {
+  res.render('facultyhome');
+});
 
+app.get('/Student/Home', function (req, res) {
+  res.render('facultyhome');
+});
 
+app.get('/LoginFailed', function (req, res) {
+  res.render('LoginFailed');
+});
 
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+app.get('/registerfaculty', function (req, res) {
+  res.render('registerfaculty');
+});
+
+app.get('/registersuccess', function (req, res) {
+  res.render('registersuccess');
+});
+
+app.get('/regiinvalid', function (req, res) {
+  res.render('regiinvalid');
+});
+
+app.get('/registerstudent', function (req, res) {
+  res.render('registerstudent');
+});
+
+app.get('/adminclasses', function (req, res) {
+  res.render('adminclass');
+});
+
+app.get('/facultyclasses', function (req, res) {
+  res.render('facultyclass');
+});
+
+app.get('/studentclasses', function (req, res) {
+});
+
+app.post('/registudent', function (req, res) {
+  var usertype = 'Student';
+  var values = [];
+  values = [req.body.firstname, req.body.lastname, req.body.username, req.body.password1, usertype, req.body.studentnumber];
+  var pass1 = req.body.password1;
+  var pass2 = req.body.password2;
+  if (pass1 === pass2) {
+     client.query('INSERT INTO users(first_name, last_name, username, password, usertype, studentnumber) VALUES($1, $2, $3, $4, $5, $6)', values, (err, res) => {
+        if (err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+        res.redirect('/registersuccess');
+  } else {
+    res.redirect('/regiinvalid');
+  }
+});
+
+app.post('/regifaculty', function (req, res) {
+  var usertype = 'Faculty';
+  var values = [];
+  values = [req.body.firstname, req.body.lastname, req.body.username, req.body.password1, usertype, req.body.Privelege];
+  var pass1 = req.body.password1;
+  var pass2 = req.body.password2;
+  if (pass1 === pass2) {
+     client.query('INSERT INTO users(first_name, last_name, username, password, usertype, isadmin) VALUES($1, $2, $3, $4, $5, $6)', values, (err, res) => {
+        if (err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+        res.redirect('/registersuccess');
+  } else {
+    res.redirect('/regiinvalid');
+  }
+});
+
+app.post('/checkusertype', function (req, res) {
+  var values = [];
+  values = [req.body.username, req.body.password];
+  var values1 = req.body.username;
+  var values2 = req.body.password;
+  var usertype = 'Faculty';
+  var isadmin = 'Admin';
+  console.log(req.body);
+  client.query('SELECT username FROM users', (req, data) => {
+    var list;
+    var exist = 0;
+    console.log('compareusername');
+    for (var i = 0; i < data.rows.length; i++) {
+      list = data.rows[i].username;
+      console.log(list);
+      if (list == values1) {
+        exist = 1;
+      }
+    }
+    if (exist === 1) {
+      console.log('usernamevalid');
+      client.query('SELECT password FROM users WHERE username = $1',[values1], (req, data) => {
+        var list1;
+        var exist1 = 0;
+        console.log('comparepassword');
+        for (var i = 0; i < data.rows.length; i++) {
+        list1 = data.rows[i].password;
+        console.log(list1);
+        if (list1 == values2) {
+        exist1 = 1;
+      }
+    }
+    if (exist1 === 1) {
+      console.log('LoginSucess');
+      console.log('Checking Usertype');
+      client.query('SELECT usertype, isadmin FROM users WHERE username = $1',[values1], (req, data) => {
+        var usertypequery = data.rows[0].usertype;
+        var isadminquery = data.rows[0].isadmin;
+        if (usertypequery == usertype) {
+          if (isadminquery == isadmin) {
+            res.redirect('/Faculty/Admin/Home');
+          } else{
+            res.redirect('/Faculty/Regular/Home');
+          }
+
+        } else {
+            res.redirect('Student/Home');
+        }
+      });
+    } else {
+      res.redirect('/LoginFailed');
+    }
+      });
+    } else {
+      res.redirect('/LoginFailed');
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log('Server started at port 3000');
--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
   "username" VARCHAR(80),
-  "email" VARCHAR(80),
   "first_name" VARCHAR(80),
   "last_name" VARCHAR(80),
   "usertype" VARCHAR(80),
+  "studentnumber" VARCHAR(80),
   "isadmin" VARCHAR(80),
   "password" VARCHAR(20)
 );
@@ -16,5 +16,8 @@ CREATE TABLE "classes" (
   "student_id" VARCHAR(80)
 );
 
+insert into users (username, first_name, last_name, isadmin, password, usertype) values ('Gtria', 'Gino', 'Tria', 'Admin', 'admin123', 'Faculty');
 
-insert into users (username, email, first_name, last_name, isadmin, password) values ('jltan5', 'tanjohnloyd3@gmail.com', 'John Loyd', 'Tan', 'Yes', 'team19password');
+insert into users (username, first_name, last_name, isadmin, password, usertype) values ('Csawi', 'Christopher', 'Sawi', 'Regular', '123admin', 'Faculty');
+
+insert into users (username, first_name, last_name, studentnumber, password, usertype) values ('Esomido', 'Ellaine', 'Somido', '2014-04010-MN-0', 'johnloyd', 'Student');
